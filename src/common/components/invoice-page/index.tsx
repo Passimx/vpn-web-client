03f41a512d9a2fe8f2998ba0142831574982a728
ramzini22 <ramil.zinnatullin.02@mail.ru>
2026-08-12 22:43:38 +0800

    refactor: add put money error

--- src/common/components/invoice-page/index.tsx
+++ src/common/components/invoice-page/index.tsx
@@ -2,19 +2,25 @@ import { FC, useEffect, useState } from 'react';
 import styles from './index.module.css';
 import { PropsType } from './types/props.type.ts';
 import { QrCode } from '../qr-code';
-import { useAppSelector } from '../../store';
+import { useAppAction, useAppSelector } from '../../store';
 import { Link } from '../link';
 import { shortText } from '../../hooks/short-text.ts';
+import { EventsEnum } from '../../types/events/events.enum.ts';
 
 export const InvoicePage: FC<PropsType> = ({ request }) => {
     const userId = useAppSelector((state) => state.app.user?.id);
     const [url, setUrl] = useState<string>();
     const shortUserId = shortText(userId);
+    const { setStateApp, postMessage } = useAppAction();
 
     useEffect(() => {
         const getResponse = async () => {
             const result = await request;
-            if (!result?.length) return;
+            if (!result) {
+                setStateApp({ foreground: undefined });
+                postMessage({ event: EventsEnum.SHOW_TEXT, data: 't0' });
+                return;
+            }
 
             setUrl(result);
         };
