a818f9bf7839b31d23eb4e06f5c19d39d802edcb
ramzini22 <ramil.zinnatullin.02@mail.ru>
2026-08-30 14:06:43 +0800

    refactor: add 2 steps

--- src/common/pages/download-pwa/index.tsx
+++ src/common/pages/download-pwa/index.tsx
@@ -4,11 +4,20 @@ import { useTranslation } from 'react-i18next';
 import iphone from '../../../../public/assets/images/iphone.png';
 import { Image } from '../../components/image';
 import pxicon from '../../../../public/assets/icons/256.png';
-import { IoShareOutline } from 'react-icons/io5';
+import { IoCopyOutline, IoShareOutline } from 'react-icons/io5';
 import { CiSquarePlus } from 'react-icons/ci';
+import { useAppAction } from '../../store';
+import { EventsEnum } from '../../types/events/events.enum.ts';
 
 export const DownloadPwa: FC = () => {
     const { t } = useTranslation();
+    const { postMessage } = useAppAction();
+
+    const copyHost = () => {
+        window.navigator.clipboard.writeText(window.location.host);
+
+        postMessage({ event: EventsEnum.SHOW_TEXT, data: 't10' });
+    };
 
     return (
         <div className={styles.div0}>
@@ -37,6 +46,16 @@ export const DownloadPwa: FC = () => {
                             {t('t100')}&#160;PassimX VPN&#160;{t('t101')}
                         </div>
                         <div className={styles.div322}>
+                            <div className={styles.div3221} onClick={copyHost}>
+                                <div className={styles.div32211}>{t('t107')}</div>
+                                <div className={`${styles.div32212} ${styles.div32211}`}>
+                                    &#160;{window.location.host}
+                                </div>
+                                <IoCopyOutline className={'icon'} />
+                            </div>
+                            <div className={styles.div3221}>
+                                <div className={styles.div32211}>{t('t108')}</div>
+                            </div>
                             <div className={styles.div3221}>
                                 <div className={styles.div32211}>{t('t102')}</div>
                                 <IoShareOutline className={styles.div32212} />
